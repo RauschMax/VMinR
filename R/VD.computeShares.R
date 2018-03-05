370f7652eb9d1c5cae6b48efbf13e355bf7acd36
Rausch <Maximilian.Rausch@kantartns.com>
2018-03-05 12:12:09 +0100

    typo removed from VD.computeShares

--- R/VD.computeShares.R
+++ R/VD.computeShares.R
@@ -3,13 +3,13 @@
 
 
 #' Compute ValueDriver shares
-#' 
+#'
 #' Calculates the shares (first choice or preference share) for a ValueDriver
 #' like study.
-#' 
-#' 
+#'
+#'
 #' @param design A matrix containing the design including the concepts to
-#' simulate - in case it is not dummy coded use \code{dummy = TRUE} to ally
+#' simulate - in case it is not dummy coded use \code{dummy = FALSE} to apply
 #' dummy coding via \code{\link{convertSSItoDesign}}
 #' @param utils A matrix containing the utilities
 #' @param nlev A vector indicating the number of levels per attribute
@@ -24,20 +24,20 @@
 #' respondent}
 #' @author Maximilian Rausch - Maximilian.Rausch@@tns-infratest.com
 #' @examples
-#' 
+#'
 #' utils_mat <- VMinR$VDdata$utils_mat
 #' nlev <- VMinR$VDdata$nlev
 #' weight <- VMinR$VDdata$weight
 #' basecase <- as.matrix(VMinR$basecase_dummy_2)
-#' 
+#'
 #' sim_BaseR <- VD.computeShares(design = basecase,
 #'                               utils = utils_mat,
 #'                               nlev = nlev,
 #'                               weight = weight,
 #'                               FC = FALSE)
-#' 
+#'
 #' round(sim_BaseR$meanShares, 3)
-#' 
+#'
 #' @export VD.computeShares
 VD.computeShares <- function(design, utils, nlev, weight = NULL, FC = FALSE, dummy = TRUE) {
   if (!dummy) dummy_design <- as.matrix(convertSSItoDesign(design, nlev = nlev))
